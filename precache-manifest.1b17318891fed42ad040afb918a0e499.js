0fb7c6b22581d4eb62a18ba14c94620c78a4e7c9
Mordecai210 <mordecai210@users.noreply.github.com>
2020-08-16 15:30:39 -0300

    Updates

---
+++ precache-manifest.1b17318891fed42ad040afb918a0e499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe87e748be1dcde9e339846a6969ef2",
+    "url": "/rich-text/index.html"
+  },
+  {
+    "revision": "2bd6afeae62a58053c02",
+    "url": "/rich-text/static/css/main.0dd23809.chunk.css"
+  },
+  {
+    "revision": "d94d778f30908af624f2",
+    "url": "/rich-text/static/js/2.8da33969.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rich-text/static/js/2.8da33969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd6afeae62a58053c02",
+    "url": "/rich-text/static/js/main.03576907.chunk.js"
+  },
+  {
+    "revision": "4955a005a66a16cc5887",
+    "url": "/rich-text/static/js/runtime-main.abb79717.js"
+  }
+]);
---
+++ static/js/main.03576907.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprich-text"]=this["webpackJsonprich-text"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var r=n(0),c=n.n(r),a=n(2),i=n.n(a);n(8);var o=function(){return c.a.createElement("div",{className:"App"},"Text")};i.a.render(c.a.createElement(c.a.StrictMode,null,c.a.createElement(o,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.03576907.chunk.js.map
